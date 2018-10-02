4c66809b29b95b79404c648b2595b1bc7eb7a568
Mikael Abehsera <Mikael.abehsera@gmail.com>
2018-10-02 12:30:18 -0400

    url shortning part 1: added a generate random string function and a route for /url/new with a input form

--- express_server.js
+++ express_server.js
@@ -1,10 +1,27 @@
 // SETUP 
-const express = require("express");
-const app = express();
-const PORT = 3000;
+const express     = require("express");
+const app         = express();
+const PORT        = 3000;
+const bodyParser  = require("body-parser");
 // SET UP 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+/*----------------------------------------------------------------------------------------------------*/
+function random(min, max) {
+  return Math.floor(Math.random() * max) + min;  
+}
+
+function generateRandomString() {
+  const alphaNum = "abcdefghigklmnopqrstuvwxyz1234567890";
+  let str = "";
+  for(let i = 0; i <= 6; i++) {
+    str += alphaNum[random(0, 35)];
+  }
+  return str;
+}
+
+/*----------------------------------------------------------------------------------------------------*/
 
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -14,26 +31,43 @@ let urlDatabase = {
 /* this is the root (aka /) route and will display hello a message */
 app.get("/", (req, res) => {
   res.send("Hello! root route is working!");
+  console.log("/ route has been accessed");
 });
 
 /* this is the /urls route and will display urls*/
 app.get("/urls", (req, res) => {
   res.render("urls_index", { urls: urlDatabase });
+  console.log("/urls route has been accessed");
+});
+
+/* this post will get the input from the /urls/new input form */ 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+/* This route will have a form to input a url */
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+  console.log("/urls/new route has been accessed");
+});
+
+app.get("/urls/:id", (req, res) => {
+  res.render("urls_show", { shortURL: req.params.id });
+  console.log("/urls/:id route has been accessed");
 });
 
 /* this is the /hello route and will display "hello world"*/
 app.get("/hello", (req, res) => {
   let templateVars = { greeting: "Hello World!"};
   res.render("hello_world", templateVars);
-});
-
-app.get("/urls/:id", (req, res) => {
-  res.render("urls_show", { shortURL: req.params.id });
+  console.log("/hello route has been accessed");
 });
 
 /* this will display a not found message for any routes we have not found */
 app.get("/*", (req, res) => {
   res.send("<html><body><h1>PAGE NOT FOUND</h1></body></html>\n");
+  console.log("Page not found");
 });
 
 app.listen(PORT, () => {
