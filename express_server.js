b61da0d4a9dfd00097a607110e68126894189c53
Mikael Abehsera <Mikael.abehsera@gmail.com>
2018-10-03 20:37:40 -0400

    added a redirect for login and made the email for login work

--- express_server.js
+++ express_server.js
@@ -1,23 +1,25 @@
 // SETUP 
-const express     = require("express");
-const app         = express();
-const PORT        = 3000;
-const bodyParser  = require("body-parser");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 // SET UP 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(cookieParser());
 app.set("view engine", "ejs");
 
 /*----------------------------------------------------------------------------------------------------*/
 function random(min, max) {
-  return Math.floor(Math.random() * max) + min;  
+  return Math.floor(Math.random() * max) + min;
 }
 
-function generateRandomString() {
+function generateRandomId() {
   const alphaNum = "abcdefghigklmnopqrstuvwxyz1234567890";
   let str = "";
-  for(let i = 0; i <= 6; i++) {
+  for (let i = 0; i <= 6; i++) {
     str += alphaNum[random(0, 35)];
   }
   return str;
@@ -33,9 +35,20 @@ let urlDatabase = {
   "23fr43": "http://www.youtube.com"
 };
 
-let user = null;
+let currentUser = null;
 
-let users = {};
+const users = {
+  "h5w2hr32r4": {
+    id: "h5w2hr32r4",
+    email: "user1@example.com",
+    password: "password123"
+  },
+  "h24hr22hf4": {
+    id: "h24hr22hf4",
+    email: "user2@example.com",
+    password: "123"
+  }
+};
 
 /* this is the root (aka /) route and will display hello a message */
 app.get("/", (req, res) => {
@@ -44,48 +57,77 @@ app.get("/", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  res.render("urls_register", {user: user, users: users});
+  res.render("urls_register", {
+    currentUser: currentUser,
+    users: users
+  });
 });
 
 app.post("/register", (req, res) => {
-  
+  const randomId = generateRandomId();
+  users[randomId] = {
+    id: randomId,
+    email: req.body.email,
+    password: req.body.password
+  };
+  currentUser = users[randomId]["email"];
+  // PASSWORD
+  console.log("PASSWORD: ", req.body.password);
+  res.redirect("/register");
 });
 
 app.get("/login", (req, res) => {
-  res.render("urls_login", {user: user});
+  res.render("urls_login", {
+    currentUser: currentUser
+  });
 });
 
 app.post("/login", (req, res) => {
-  console.log("BEFORE: ", user);
-  user = req.body.user;
-  res.cookie("user", user);
-  console.log("cookie: ", req.cookies.user);
-  res.redirect("/login");
+  // res.cookie("currentUser", currentUser);
+  let currentId;
+  for (let id in users) {
+    if(req.body.user === users[id]["email"]) {
+      currentId = id;
+    }
+  }
+  console.log(currentId);
+  if(currentId === undefined) {
+    res.redirect("/register");
+  } else {
+    currentUser = users[currentId]["email"];
+    res.redirect("/login");
+  }
 });
 
 app.post("/logout", (req, res) => {
-  console.log("User logging out!")
-  user = null;
-  res.clearCookie(user);
+  console.log("User logging out!");
+  currentUser = null;
+  res.clearCookie(currentUser);
   res.redirect("../urls");
 });
 
 /* this is the /urls route and will display urls*/
 app.get("/urls", (req, res) => {
-  res.render("urls_index", { urls: urlDatabase, user: user });
+  res.render("urls_index", {
+    urls: urlDatabase,
+    currentUser: currentUser
+  });
   console.log("/urls route has been accessed");
 });
 
-/* this post will get the input from the /urls/new input form */ 
-app.post("/urls", (req, res) => {   
-  let randomString = generateRandomString();
+/* this post will get the input from the /urls/new input form */
+app.post("/urls", (req, res) => {
+  let randomString = generateRandomId();
   urlDatabase[randomString] = req.body.longURL;
   res.redirect(`/urls/${randomString}`);
 });
 
 /* This route will have a form to input a url */
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new", {user: user});
+  res.render("urls_new", {
+  currentUser: currentUser
+
+  });
   console.log("/urls/new route has been accessed");
 });
 
@@ -102,7 +144,10 @@ app.post("/urls/:id/update", (req, res) => {
 
 
 app.get("/urls/:id", (req, res) => {
-  res.render("urls_show", { shortURL: req.params.id, user: user});
+  res.render("urls_show", {
+    shortURL: req.params.id,
+    currentUser: currentUser
+  });
   console.log("/urls/:id route has been accessed");
 });
 
@@ -113,7 +158,9 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 /* this is the /hello route and will display "hello world"*/
 app.get("/hello", (req, res) => {
-  let templateVars = { greeting: "Hello World!"};
+  let templateVars = {
+    greeting: "Hello World!"
+  };
   res.render("hello_world", templateVars);
   console.log("/hello route has been accessed");
 });
